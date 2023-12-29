85617da7f7a444acfc7a2caa2661c2ab47060b16
namah-age <sosakaguchi@g.ecc.u-tokyo.ac.jp>
2023-12-29 10:12:07 -0500

    ci test added

---
+++ test/test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Simple Test Suite', () => {
+  it('True should be true', () => {
+    expect(true).to.be.true;
+  });
+
+  // Flaky test
+  it('Flaky Test', () => {
+    expect(Math.random() < 0.5).to.be.true; // This test will fail about 50% of the time
+  });
+});
+
